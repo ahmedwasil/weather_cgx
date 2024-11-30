321ec00c50cefd9cc99bafb6705efbcf796570c1
Wasil Ahmed <ahmed.wasil@gmail.com>
2024-11-30 23:10:42 +0000

    Initial Commit

---
+++ weather_cgx/weather_cgxApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_cgxApp.swift
+//  weather_cgx
+//
+//  Created by Wasil Ahmed on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_cgxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather_cgxTests/weather_cgxTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_cgxTests.swift
+//  weather_cgxTests
+//
+//  Created by Wasil Ahmed on 30/11/2024.
+//
+
+import Testing
+@testable import weather_cgx
+
+struct weather_cgxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
